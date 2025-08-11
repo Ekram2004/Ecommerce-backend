95345a7bb6bafc290acc2ee86c0700591e58ff57
Ekram Asrar <ekramasrar94@gmail.com>
2025-08-11 10:13:16 -0700

    adding cart model and endpoints

---
+++ controllers/cartControllers.js
@@ -0,0 +1,48 @@
+const Cart = require('../models/Cart');
+const Product = require('../models/Product');
+
+
+// Get user cart
+
+exports.getCart = async (req, res) => {
+    try {
+        const cart = await Cart.findOne({ user: req.user.id }).populate('items.product');
+        if (!cart) return res.json({ items: [] });
+        res.json(cart);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server error' });
+    }
+};
+
+exports.addToCart = async (req, res) => {
+    try {
+        const { productId, quantity } = req.body;
+        const product = await Product.findById(productId);
+        if (!product)
+            return res.status(404).json({ message: 'Product not found' });
+        let cart = await Cart.findOne({ user: req.user.id, items: [] });
+        if (!cart) {
+            cart = new Cart({ user: req.user.id, items: [] });
+        }
+
+        const itemIndex = cart.items.findIndex(item => item.product.toString() === productId);
+        if (itemIndex > -1) {
+            cart.items[itemIndex].quantity += quantity || 1;
+        } else {
+            cart.items.push({ product: productId, quantity: quantity || 1 });
+        }
+        await cart.save();
+        res.json(cart);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server error' });
+    }
+}
+
+exports.updateCartItem = async (req, res) => {
+    try {
+        const { quantity } = req.body;
+        const cart = await Cart.findOne({ user: req.user.id });
+    }
+}
